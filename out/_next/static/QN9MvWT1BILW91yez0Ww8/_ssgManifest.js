f4c700c788d524b29e000b01b35dfb10ca1a73a5
Balachander Sathianarayanan <sabalachander.off@gmail.com>
2026-02-13 10:00:00 -0500

    Remove out/ from tracking

--- out/_next/static/QN9MvWT1BILW91yez0Ww8/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcontact","\u002Fexperience","\u002Fprojects","\u002Fprojects\u002F[slug]","\u002Fpublications"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
